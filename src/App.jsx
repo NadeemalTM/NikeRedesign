3595ebdeb3add2d83212d612bcbc4f501535fade
T.M.Gayantha Nadeemal <136301368+NadeemalTM@users.noreply.github.com>
2025-08-07 16:17:35 +0530

    Second

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import HeroCarousel from "./components/HeroCarousel";
 import "./App.css";
 
 function App() {
   return (
-    <div className="main-gray-bg" style={{ minHeight: "100vh", width: "100vw" }}>
+    <div className="main-gray-bg">
       <Header />
+      <HeroCarousel />
       {/* Add your main content here */}
     </div>
   );
