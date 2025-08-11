d6cbf24e57cd7ae49173d27fe90c088e2b5ef839
T.M.Gayantha Nadeemal <136301368+NadeemalTM@users.noreply.github.com>
2025-08-11 15:46:48 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
-import HeroCarousel from "./components/HeroCarousel";
+import HeroBody from "./components/HeroBody";
+import HomeBody from "./components/HomeBody";
 import "./App.css";
 
 function App() {
   return (
-    <div className="main-gray-bg">
+    <>
       <Header />
-      <HeroCarousel />
-      {/* Add your main content here */}
-    </div>
+      <main className="main-content">
+        <HeroBody />
+        <HomeBody />
+      </main>
+    </>
   );
 }
 
